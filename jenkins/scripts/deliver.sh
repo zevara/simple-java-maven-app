9c6c5604208a09f474ca0383ae8dd846c752c086
vagrant <vagrant@Jenkins>
2022-10-16 03:17:33 +0000

    add timeout

--- jenkins/scripts/deliver.sh
+++ jenkins/scripts/deliver.sh
@@ -23,6 +23,7 @@ set +x
 echo 'The following command runs and outputs the execution of your Java'
 echo 'application (which Jenkins built using Maven) to the Jenkins UI.'
 set -x
-java -jar target/${NAME}-${VERSION}.jar
+java -jar target/${NAME}-${VERSION}.jar &
+sleep 1m
 echo $! > .pidfile
 set +x
