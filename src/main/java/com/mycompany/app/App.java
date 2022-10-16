005bfe5e22fb79e07949a72a83778bc2ec158375
vagrant <vagrant@Jenkins>
2022-10-16 15:08:29 +0000

    change java

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -6,15 +6,15 @@ package com.mycompany.app;
 public class App
 {
 
-    // private final String message = "Hello World!";
+    private final String message = "Hello World!";
 
     public App() {}
 
     public static void main(String[] args) {
 
-        System.out.println("Hello World!");    
+        // System.out.println("Hello World!");    
 
-        // System.out.println(new App().getMessage());
+        System.out.println(new App().getMessage());
     }
 
     private final String getMessage() {
