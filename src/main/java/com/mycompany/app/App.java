4f549f42e8238c791a987926dca6440a3d706efa
vagrant <vagrant@Jenkins>
2022-10-16 04:52:21 +0000

    add java

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -1,21 +1,32 @@
-package com.mycompany.app;
+// package com.mycompany.app;
 
-/**
- * Hello world!
- */
-public class App
-{
+// /**
+//  * Hello world!
+//  */
+// public class App
+// {
 
-    private final String message = "Hello World!";
+//     private final String message = "Hello World!";
 
-    public App() {}
+//     public App() {}
 
-    public static void main(String[] args) {
-        System.out.println(new App().getMessage());
-    }
+//     public static void main(String[] args) {
+//         System.out.println(new App().getMessage());
+//     }
 
-    private final String getMessage() {
-        return message;
-    }
+//     private final String getMessage() {
+//         return message;
+//     }
 
+// }
+
+package com.codacuum.springboothelloworld.controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RESTController;
+@RESTController
+public class HelloWorldController {
+@GetMapping(“/hello-world”)
+public String HelloWorld(){
+return “Hello World”;
+}
 }
