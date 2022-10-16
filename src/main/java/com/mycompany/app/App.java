8c553a988f72f0f8adaf0272c2b127dcff0d19e9
vagrant <vagrant@Jenkins>
2022-10-16 11:32:10 +0000

    change java

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -12,7 +12,7 @@ public class App
 
     public static void main(String[] args) {
         System.out.println(new App().getMessage());
-        Thread.sleep(10000);
+        Thread.sleep(1200000);
          }
 
     private final String getMessage() {
